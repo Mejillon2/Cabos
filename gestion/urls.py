4cd467f1cae2cd5af2486f3fcf2fc9d86f433f0d
Mejillon2 <joseale2805@gmail.com>
2026-04-29 21:18:31 -0600

    vistas

--- gestion/urls.py
+++ gestion/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login_view'),
     path('logout/', views.logout_view, name='logout_view'),
+    path('terapeutas/', views.lista_terapeutas, name='lista_terapeutas'),
     path('pacientes/', views.lista_pacientes, name='lista_pacientes'),
+    path('caballos/', views.lista_caballos, name='lista_caballos'),
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -62,3 +62,44 @@ def lista_pacientes(request):
     conn.close()
     
     return render(request, 'gestion/pacientes.html', {'pacientes': pacientes})
+
+
+def lista_terapeutas(request):
+    if 'admin_id' not in request.session: return redirect('login_view')
+    conn = get_connection()
+    cur = conn.cursor()
+    query = """
+        SELECT p.ID_PERSONA, p.NOMBRE, t.ESPECIALIDAD 
+        FROM PERSONAS p
+        JOIN TERAPEUTAS_INFO t ON p.ID_PERSONA = t.ID_TERAPEUTA
+    """
+    cur.execute(query)
+    terapeutas = cur.fetchall()
+    conn.close()
+    return render(request, 'gestion/terapeutas.html', {'terapeutas': terapeutas})
+
+def lista_caballos(request):
+    if 'admin_id' not in request.session: return redirect('login_view')
+    conn = get_connection()
+    cur = conn.cursor()
+    query = """
+        SELECT c.NOMBRE_C, c.RAZA, c.ESTADO_DISPONIBILIDAD, s.ESTADO_FISICO
+        FROM CABALLOS c
+        LEFT JOIN SALUD_CABALLO s ON c.ID_CABALLO = s.ID_CABALLO
+    """
+    cur.execute(query)
+    caballos = cur.fetchall()
+    conn.close()
+    return render(request, 'gestion/caballos.html', {'caballos': caballos})
+
+def lista_padres(request):
+    if 'admin_id' not in request.session: 
+        return redirect('login_view')
+    
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT ID_PERSONA, NOMBRE, TELEFONO, EMAIL FROM PERSONAS WHERE ROL = 'Padre'")
+    padres = cur.fetchall()
+    conn.close()
+    
+    return render(request, 'gestion/padres.html', {'padres': padres})
